4f9adf92fc183528035e646edda5a7266c200670
Aladin Scott <zimbabalim@Aladins-MacBook-Pro.local>
2017-12-17 22:30:26 +0000

    add hopkins files

--- server.js
+++ server.js
@@ -1,108 +1,231 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
+var express = require( "express" );
+var fs = require( "fs" );
+var path = require( "path" );
+var routes = require( "./routes/index" );
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+var mongo = require('mongodb');
+var monk = require('monk');
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+var bb = require('express-busboy');
+// var fileUpload = require('express-fileupload');
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
+// var bodyParser = require('body-parser');
 
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
 
-  }
-}
-var db = null,
-    dbDetails = new Object();
 
-var initDb = function(callback) {
-  if (mongoURL == null) return;
+var App = function(){
 
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
+    var _this = this;
 
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
+    _this.initIP = function(){
 
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
+        _this.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        _this.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
+        if (typeof _this.ipaddress === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            _this.ipaddress = "127.0.0.1";
+        }
+    };
+
+
+    // *** NOTE not calling this at the moment, but worth considering working it out
+    _this.initCache = function(){
+
+        console.log("/server/ -initCache ");
+
+        if (typeof _this.cache === "undefined") {
+            _this.cache = { 'index.html': '' };
+        }
+
+        //  Local cache for static content.
+        _this.cache['index.html'] = fs.readFileSync('./index.html');
+
+    };
+
+
+    _this.cache_get = function( _key ) {
+        return _this.cache[ _key ];
+    };
+
+
+    _this.terminator = function( _sig ){
+        if (typeof _sig === "string") {
+            console.log('%s: Received %s - terminating sample app ...',  Date( Date.now()), _sig );
+            process.exit( 1 );
+        }
+        console.log('%s: Node server stopped.', Date( Date.now() ) );
+    };
+
+    _this.setupTerminationHandlers = function(){
+        process.on('exit', function() { _this.terminator(); });
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'].forEach(function(element, index, array) {
+            process.on( element, function() { _this.terminator( element ); });
+        });
+    };
+
+
+
+
+    _this.initServer = function() {
+
+        _this.app = express(); // *** fix for above being deprecated
+
+        _this.app.engine('html', require('ejs').renderFile);
+        _this.app.use( express.static( path.join( __dirname, "public" )));
+
+        // _this.app.use(fileUpload());
+
+        bb.extend( _this.app, {
+            upload : true,
+            path: "./public/_upload-temp"
+        } );
+
+
+        /*bb.extend( _this.app, {
+            upload: true,
+            path: "./public"
+        });*/
+
+        //allowedPath : /.FOO$/
+
+
+        // bb.options.allowedPath = function(url) {
+        //     return url == '/FOO';
+        // };
+
+        // _this.app.use( bb );
+
+
+        // _this.app.use( bodyParser.json() ); // support json encoded bodies
+        // _this.app.use( bodyParser.urlencoded({ extended: true }) ); // support encoded bodies
+
+        // Make our db accessible to our router
+        _this.app.use(function( req, res, next ){
+            req.db = _this.db;
+            next();
+        });
+
+        _this.app.use( "/", routes); // RESTORE
+
+
+
+
+
+        /**
+         * FIXIT not sure if this is stopping js routing - will need to investigate
+         */
+        /*_this.app.use(function(req, res, next) {
+         /!*var err = new Error('Not Found');
+         err.status = 404;
+         next(err);*!/
+
+         res.render( "index.ejs" ); // *** NOTE go to index page on 404 - TODO better handling or not bother?
+         });*/
+
+
+        // REMOVE - testing original framework in situ
+        _this.app.get("/test", function( req, res ){
+            // res.render( "page3.html" );
+            res.sendFile( __dirname + "/public/test-delete/index.html" );
+        });
+
+
+
+        /*_this.app.all('*', function(req, res, next) {
+         //console.log("/server3/ - ", req );
+         //res.sendFile( __dirname + "/dist/index.html" );
+
+         console.log("/server/ - ALL?");
+
+         res.render( "index.ejs", { foo : "mofo folk bonobo zooboodoo" } );
+
+         });*/
+
+
+
+
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+        if (_this.app.get('env') === 'development') {
+            _this.app.use(function(err, req, res, next) {
+                res.status(err.status || 500);
+                res.render('error.ejs', {
+                    message: err.message,
+                    error: err
+                });
+            });
+        }
+
+// production error handler
+// no stacktraces leaked to user
+        _this.app.use(function(err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error.ejs', {
+                message: err.message,
+                error: {}
+            });
+        });
+
+
+
+    };
+
+
+    _this.init = function() {
+        _this.initIP();
+        //_this.initCache(); // *** TODO might need this!
+        _this.setupTerminationHandlers();
+
+        // Create the express server and routes.
+        _this.initMongoDB(); // TEST ONLY!!!
+        _this.initServer();
+
+    };
+
+
+    _this.start = function() {
+        //  Start the app on the specific interface (and port).
+        _this.app.listen(_this.port, _this.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                Date(Date.now() ), _this.ipaddress, _this.port);
+        });
+    };
+
+
+    _this.initMongoDB = function(){
+
+        var connection_string = "localhost:27017/centrepede-test";
+
+        if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+            connection_string =
+                process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+                process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+                process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+                process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+                process.env.OPENSHIFT_APP_NAME;
+        }
+
+        console.log("/server/ -initMongoDB --connection_string", connection_string );
+
+        // *** FIXIT this might be occasional causing startup failure - 200816 : actually i think it's chrome cookies not being ready
+        // *** NOTE USEFUL TO CHECK DB CONNECTED!
+        /*_this.db = monk( connection_string ); // *** db path
+         var foobar = _this.db.get("foobar");
+
+         var r = foobar.find({}, function( _error, _data ){
+         console.log("/server/ - FIND", _error, _data );
+         });*/
+
+        _this.db = monk( connection_string ); // *** db path NOTE open connection?
+    };
 };
 
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
-
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
+var app = new App();
+app.init();
+app.start();
